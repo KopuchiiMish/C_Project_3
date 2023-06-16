fd7521e8ab72abff9494a9906a1830114ee76c52
nataliagovorushkina <a10061514@gmail.com>
2023-06-16 16:56:21 +0300

    adding docstrings to main.py

--- main/main.py
+++ main/main.py
@@ -2,16 +2,22 @@ from utils.utils import load_operations, get_operation_by_state, sort_by_date, c
     hide_account_number
 
 operations_filename = 'operations.json'
+'''Обозначение имени изначального файла со всеми операциями клиента'''
 
 load_operations = load_operations(operations_filename)
+'''Загружаем файл для дальнейшей обработки'''
 
 s = "EXECUTED"
 
 executed_list = get_operation_by_state(s, load_operations)
+'''Обозначаем первый параметр, по которому отбираем операции из изначального файла 
+и создаем из них новый список'''
 
 sorted_list = sort_by_date(executed_list)
+'''Сортируем операции в хронологическом порядке'''
 
 five_latest_operations = sorted_list[:5]
+'''Отделяем 5 последних операций в отдельный список'''
 
 for transaction in five_latest_operations:
     date = transaction.get("date")
@@ -20,6 +26,7 @@ for transaction in five_latest_operations:
     to_ = transaction.get("to")
     amount = transaction.get("operationAmount").get("amount")
     currency = transaction.get("operationAmount").get("currency").get("name")
+    '''Извлекаем нужные данные по ключам'''
 
     date = convert_date(date)
     if not from_:
@@ -33,8 +40,11 @@ for transaction in five_latest_operations:
         hidden_from_number = hide_card_number(from_[-16:])
 
     to_ = hide_account_number(to_[-20:])
+    '''Частично маскируем номера счетов и карт'''
 
     print(f'''{date} {description}
 {name_from_} {hidden_from_number} -> Счет {to_}
 {amount} {currency}
 ''')
+'''Выводим результат'''
+
